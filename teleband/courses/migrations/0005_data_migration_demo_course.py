76de5f2aecc586519ca2507d924d3a0ec80f3207
Michael Stewart <stewarmc@jmu.edu>
2022-01-14 10:36:47 -0500

    seed demo course and users

--- teleband/courses/migrations/0005_data_migration_demo_course.py
+++ teleband/courses/migrations/0005_data_migration_demo_course.py
@@ -4,40 +4,45 @@ from django.db import migrations
 from datetime import date
 
 
+def demo_course(apps, schema_editor):
+    User = apps.get_model("users", "User")
+
+    # how to import this from the other migrations file?
+    michael = User.objects.filter(email="michael@tele.band")[0]
+    alden = User.objects.filter(email="alden@tele.band")[0]
+    dave = User.objects.filter(email="dave@tele.band")[0]
+    Course = apps.get_model("courses", "course")
+    sixth_grade = Course.objects.update_or_create(
+        name="6th Grade Band",
+        owner=dave,
+        start_date=date(2022, 1, 9),
+        end_date=date(2022, 6, 9),
+        slug="6th-grade-band",
+    )[0]
+
+    Instrument = apps.get_model("instruments", "Instrument")
+    trombone = Instrument.objects.filter(name="Trombone")[0]
+    Role = apps.get_model("users", "Role")
+    teacher = Role.objects.filter(name="Teacher")[0]
+    student = Role.objects.filter(name="Student")[0]
+
+    Enrollment = apps.get_model("courses", "enrollment")
+
+    Enrollment.objects.update_or_create(user=dave, course=sixth_grade, role=teacher)
+    Enrollment.objects.update_or_create(
+        user=michael, course=sixth_grade, instrument=trombone, role=student
+    )
+
+    Enrollment.objects.update_or_create(
+        user=alden, course=sixth_grade, instrument=trombone, role=student
+    )
+
+
 class Migration(migrations.Migration):
-    def demo_course(apps, schema_editor):
-        User = apps.get_model("users", "User")
-
-        michael = User.objects.filter(email="michael@tele.band")[
-            0
-        ]  # how to import this from the other migrations file?
-        Course = apps.get_model("courses", "course")
-        sixth_grade = Course.objects.update_or_create(
-            name="6th Grade Band",
-            owner=michael,
-            start_date=date(2022, 1, 9),
-            end_date=date(2022, 6, 9),
-        )[0]
-
-        Instrument = apps.get_model("instruments", "Instrument")
-        trombone = Instrument.objects.filter(name="Trombone")[0]
-        Role = apps.get_model("users", "Role")
-        teacher = Role.objects.filter(name="Teacher")[0]
-        student = Role.objects.filter(name="Student")[0]
-        Enrollment = apps.get_model("courses", "course")
-        Enrollment.objects.update_or_create(
-            user=michael, course=sixth_grade, instrument=trombone, role=student
-        )
-        alden = User.objects.filter(email="alden@tele.band")[0]
-        Enrollment.objects.update_or_create(
-            user=alden, course=sixth_grade, instrument=trombone, role=student
-        )
-        dave = User.objects.filter(email="dave@tele.band")[0]
-        Enrollment.objects.update_or_create(user=dave, course=sixth_grade, role=teacher)
 
     dependencies = [
         ("courses", "0004_alter_enrollment_unique_together"),
         ("users", "0004_data_migration_demo_users"),
     ]
 
-    operations = []
+    operations = [migrations.RunPython(demo_course, migrations.RunPython.noop)]
--- teleband/users/migrations/0004_data_migration_demo_users.py
+++ teleband/users/migrations/0004_data_migration_demo_users.py
@@ -4,27 +4,29 @@ from django.db import migrations
 from django.contrib.auth.hashers import make_password
 
 
+def demo_users(apps, schema_editor):
+    User = apps.get_model("users", "User")
+    User.objects.update_or_create(
+        email="michael@tele.band",
+        username="demomike",
+        name="Michael Stewart",
+        password=make_password("michael"),
+    )
+    User.objects.update_or_create(
+        email="dave@tele.band",
+        username="demodave",
+        name="Dave Stringham",
+        password=make_password("dave"),
+    )
+    User.objects.update_or_create(
+        email="alden@tele.band",
+        username="demoalden",
+        name="Alden Snell",
+        password=make_password("alden"),
+    )
+
+
 class Migration(migrations.Migration):
-    def demo_users(apps, schema_editor):
-        User = apps.get_model("users", "User")
-        User.objects.update_or_create(
-            email="michael@tele.band",
-            username="demomike",
-            name="Michael Stewart",
-            password=make_password("michael"),
-        )
-        User.objects.update_or_create(
-            email="dave@tele.band",
-            username="demodave",
-            name="Dave Stringham",
-            password=make_password("dave"),
-        )
-        User.objects.update_or_create(
-            email="alden@tele.band",
-            username="demoalden",
-            name="Alden Snell",
-            password=make_password("alden"),
-        )
 
     dependencies = [
         ("users", "0003_data_migration_student_teacher"),
