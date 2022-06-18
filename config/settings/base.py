9e3049e3aba4d8ca4ab24db48e5f705f37faedd4
Michael Stewart <stewarmc@jmu.edu>
2022-06-17 23:26:47 -0400

    default to sqlite for dev, add activity_type to existing activities

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default="sqlite:///db.sqlite"),
+    "default": env.db("DATABASE_URL", default="postgres:///teleband"),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
