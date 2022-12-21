c3a24ac6533e011628e6cd05bd3805c510c3af59
Michael Stewart <stewarmc@jmu.edu>
2022-12-21 01:14:10 -0500

    wrong var for django root

--- config/settings/production.py
+++ config/settings/production.py
@@ -163,7 +163,7 @@ LOGGING = {
         "applogfile": {
             "level": "DEBUG",
             "class": "logging.handlers.RotatingFileHandler",
-            "filename": os.path.join(DJANGO_ROOT, "logs", "musiccpr.log"),
+            "filename": os.path.join(ROOT_DIR, "logs", "musiccpr.log"),
             "maxBytes": 1024 * 1024 * 15,  # 15MB
             "backupCount": 10,
         },
