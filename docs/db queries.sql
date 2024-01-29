1529b58bfdb685e382fce07a70187cb0e8a53089
Michael Stewart <stewarmc@jmu.edu>
2024-01-29 09:37:11 -0500

    a query that was helpful in getting some data to alden

--- docs/db queries.sql
+++ docs/db queries.sql
@@ -13,3 +13,51 @@ join active_storage_attachments asa on asa.record_id = sa.id
 join active_storage_blobs asb on asb.id = asa.blob_id 
 where s.teacher_id > 13 and s.teacher_id not in (21,23,24,25,26) and asa.record_type = 'StudentAssignment';
 -- musiccpr
+
+
+--aug 2 2023
+
+-- oct 23 get alden the respond and connect things
+select
+uu.id as student_id,
+ii.name as instrument,
+uu.grade as grade,
+cc.owner_id as teacher_id,
+aa.id as assignment_id,
+p."name" as piece,
+pt."name" as part,
+actc."name" as activity_category,
+actt."name" as activity_type,
+ss.submitted as created_at,
+sa.file as recording_file,
+ss.content as content_response_composition,
+sg.tone as tone,
+sg.rhythm as rhythm,
+sg."expression" as "expression",
+ss.id as "submission_id"
+from assignments_assignment aa
+join courses_enrollment ce on aa.enrollment_id = ce.id
+join users_user uu on uu.id = ce.user_id
+join instruments_instrument ii on ii.id = ce.instrument_id
+join courses_course cc on cc.id = ce.course_id
+join assignments_activity asgn_act on aa.activity_id = asgn_act.id
+join assignments_activitytype actt on actt.id = asgn_act.activity_type_id
+join assignments_activitycategory actc on actc.id = actt.category_id 
+join musics_part mp on mp.id = aa.part_id
+join musics_piece p on p.id = mp.piece_id
+join musics_parttype pt on pt.id = mp.part_type_id
+left join submissions_submission ss on ss.assignment_id = aa.id
+left join submissions_submissionattachment sa on sa.submission_id = ss.id
+left join submissions_grade sg on sg.id = ss.grade_id
+where cc.owner_id in (9,10) and actc."name" in ('Respond', 'Connect', 'Connect Green', 'Connect Benjamin', 'Connect Danyew') and ss.content is not null and ss.id in (select max(ss.id)
+from assignments_assignment aa 
+join assignments_activity asgn_act on aa.activity_id = asgn_act.id
+join assignments_activitytype actt on actt.id = asgn_act.activity_type_id
+join assignments_activitycategory actc on actc.id = actt.category_id
+left join submissions_submission ss on ss.assignment_id = aa.id
+left join submissions_submissionattachment sa on sa.submission_id = ss.id
+left join submissions_grade sg on sg.id = ss.grade_id
+join courses_enrollment ce on ce.id =aa.enrollment_id 
+join courses_course cc on cc.id = ce.course_id 
+where cc.owner_id in (9,10) and actc."name" in ('Respond', 'Connect', 'Connect Green', 'Connect Benjamin', 'Connect Danyew') and ss.content is not null
+group by aa.id)
