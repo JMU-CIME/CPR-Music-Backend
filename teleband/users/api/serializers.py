6b9f28d0a02cefa39b9b2a0dfd2808144fe5c9ba
Michael Stewart <stewarmc@jmu.edu>
2024-03-07 22:04:31 -0500

    add user groups to /api/users/me

--- teleband/users/api/serializers.py
+++ teleband/users/api/serializers.py
@@ -1,17 +1,30 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 from teleband.instruments.api.serializers import InstrumentSerializer
+from teleband.utils.serializers import GenericNameSerializer
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     instrument = InstrumentSerializer
+    # groups = [g for g in groups.values()]
+    groups = GenericNameSerializer(model_cls=Group, many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "username", "name", "url", "grade", "instrument", "external_id"]
+        fields = [
+            "id",
+            "username",
+            "name",
+            "url",
+            "grade",
+            "instrument",
+            "external_id",
+            "groups",
+        ]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
