9fd4f311ab1a019f7c66a818f9e8ab8b4e950057
Michael Stewart <stewarmc@jmu.edu>
2024-03-19 23:34:38 -0400

    add midi program number to instrument

--- teleband/instruments/models.py
+++ teleband/instruments/models.py
@@ -13,6 +13,7 @@ class Instrument(models.Model):
 
     name = models.CharField(max_length=255)
     transposition = models.ForeignKey(Transposition, on_delete=models.PROTECT)
+    midi_program_number = models.PositiveSmallIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
